17dff4ea29fd8e18f488eb8e49b99cc93b23d4cc
AleixMT <aleixaretra@gmail.com>
2023-08-26 17:56:01 +0200

    UPDATED: installation class

--- src/models/Installation.py
+++ src/models/Installation.py
@@ -3,6 +3,13 @@
 
 class Installation:
 
+    name: str = ""
+    description: str = ""
+    version: str = ""
+    tags: 
+    pycharm_tags = ("IDE" "development" "text editor" "dev" "programming" "python")
+    pycharm_systemcategories = ("Debugger" "IDE" "WebDevelopment" "ComputerScience" "Development")
+
     def __init__(self):
         pass
 
